0e62cf077f783ff6f5e6d1c7e52381a8ba24b062
SHARON-ORTIZ <SHARON-ORTIZ@users.noreply.github.com>
2016-12-05 22:07:28 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+# Get reverse matrix:
+# The following makeCacheMatrix function is a function that caches the inverse of an array.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inversa <- NULL                                           # Create lodging variable
+        setting <- function(t) {                                  
+        x <<- t
+        inversa <<- NULL
+        }
+        getting <- function() x                                   
+        settingInversa <- function(inverse) inversa <<- inverse       
+        gettingInversa <- function() inversa                         
+        list(setting = setting, 
+                getting = getting, 
+                settingInversa = settingInversa, 
+                gettingInversa = gettingInversa)
 }
 
 
-## Write a short comment describing this function
+# The cacheSolve function calculates the inverse of the array created by 
+# makeCacheMatrix. If the inverse of the input matrix has been calculated 
+# before, the inverse is retrieved from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        inversa <- x$gettingInversa()
+        if (!is.null(inversa)) {
+        message("getting cached data")
+        return(inversa)
+        }
+  
+         mt <- x$getting()
+        if(det(mt)==0){
+        return("The matrix hasn't inverse")
+        }
+        inversa <- solve(mt, ...)                                #getting the inverse
+        x$settingInversa(inversa)
+        inversa
 }
+
+# Proof makeCacheMatrix and cacheSolve functions
+t <- makeCacheMatrix(matrix(sample(25),5,5))
+cacheSolve(t)
